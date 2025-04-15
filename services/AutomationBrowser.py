8268f4dd092301f59fb74f46727cfc66cb5118b7
Luiz Togni <contato.togni@gmail.com>
2025-04-15 16:27:32 -0300

    feat: class automation browser

---
+++ services/AutomationBrowser.py
@@ -0,0 +1,54 @@
+import os
+
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+class AutomationBrowser:
+    def __init__(self, initial_url=''):
+        options = Options()
+        options.add_argument('--start-maximized')
+        options.add_argument('--user-data-dir=/chrome_profile')
+        options.add_experimental_option("detach", True)
+
+        self._driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+        if len(initial_url) > 0:
+            self.open_page(initial_url)
+
+    def open_page(self, url):
+        self._driver.get(url)
+
+    def send_file(self, id_element, filename, delay_before=0, delay_after=0):
+        sleep(delay_before)
+        element = self._driver.find_element(By.ID, id_element)
+        element.send_keys(os.path.join(os.getcwd(), 'temp', filename))
+        sleep(delay_after)
+
+    def click_button(self, id_element, delay_before=0, delay_after=0):
+        sleep(delay_before)
+        button = self._driver.find_element(By.ID, id_element)
+        button.click()
+        sleep(delay_after)
+
+    def get_lines_table(self, id_table):
+        table = self._driver.find_element(By.ID, id_table)
+        table_body = table.find_element(By.TAG_NAME, "tbody")
+        return table_body.find_elements(By.TAG_NAME, 'tr')
+    
+    def get_value_column(self, line_element):
+        return line_element.find_elements(By.TAG_NAME, 'td')
+
+    def get_recaptcha_response(self):
+        return self._driver.execute_script('return document.getElementById("g-recaptcha-response").value')
+    
+    def set_zoom(self, value, delay_before=0, delay_after=0):
+        sleep(delay_before)
+        self._driver.execute_script(f'document.body.style.zoom="{value}%"')
+        sleep(delay_after)
+
+    def close(self):
+        self._driver.close()
